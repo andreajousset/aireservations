15a3b59d9341da056e1420931ffe6f1640611bd5
andreajousset <jousset.andrea@gmail.com>
2021-04-29 08:59:05 +0200

    intégration

---
+++ js/app.js
@@ -0,0 +1,33 @@
+let mainMenu = document.querySelector("#menu");
+
+let burgerMenu = document.querySelector("#menu-burger")
+
+/*===============================*/
+/*=== Clic sur le menu burger ===*/
+/*===============================*/
+// Vérifie si l'événement touchstart existe et est le premier déclenché
+let clickedEvent = "click"; // Au clic si "touchstart" n'est pas détecté
+window.addEventListener('touchstart', function detectTouch() {
+	clickedEvent = "touchstart"; // Transforme l'événement en "touchstart"
+	window.removeEventListener('touchstart', detectTouch, false);
+}, false);
+
+// Créé un "toggle class" en Javascrit natif (compatible partout)
+burgerMenu.addEventListener(clickedEvent, function(evt) {
+	console.log(clickedEvent);
+	// Modification du menu burger
+	if(!this.getAttribute("class")) {
+		this.setAttribute("class", "clicked");
+	} else {
+		this.removeAttribute("class");
+	}
+	// Variante avec x.classList (ou DOMTokenList), pas 100% compatible avant IE 11...
+	// burgerMenu.classList.toggle("clicked");
+
+	// Créé l'effet pour le menu slide (compatible partout)
+	if(mainMenu.getAttribute("class") != "visible") {
+		mainMenu.setAttribute("class", "visible");
+	} else {
+		mainMenu.setAttribute("class", "invisible");
+	}
+}, false);
